6a8c8ad56acaeaa5de68a789dbf98ed28119f70d
“sri-kumar” <kssrikumar180703@gmail.com>
2023-03-07 22:38:08 +0530

    responsive arenas

--- src/components/Registration/registration.js
+++ src/components/Registration/registration.js
@@ -6,7 +6,16 @@ import { Button, Form, Input, Card, Select } from "antd";
 import { Option } from "antd/es/mentions";
 import { DoubleRightOutlined, SendOutlined } from "@ant-design/icons"
 
-
+const icons = {
+    "volleyball": "fa-solid fa-volleyball",
+    "basketball": "fa-solid fa-basketball",
+    "football": "fa-regular fa-futbol",
+    "golf": "fa-solid fa-golf-ball-tee",
+    "tennis":"fa-solid fa-baseball",
+    "cricket": "fa-solid fa-baseball-bat-ball",
+    "kabbadi": "fa-solid fa-people-pulling",
+    "croquet":"fa-solid fa-gavel"
+}
 const Registration = () => {
     const [arenaId, setArenaId] = useState("");
     const [arenaName, setArenaName] = useState("SELECT ARENA");
@@ -37,14 +46,23 @@ const Registration = () => {
         console.log(e.target.value)
         setDate(e.target.value);
     }
+    const getIcon = (arName) => {
+        for (const icon in icons) {
+            if (arName.includes(icon)) {
+                return icons[icon];
+            }
+        }
+        return "fa-solid fa-medal";
+    }
+
     return (<div className="registration">
         <div className="arena-container">
             {arenaId ?
                 <div style={{ width: "90%" }}>
                     <div className="date-slots">
                         <Button size="large" className="btn-date col-md-3 ">Select Date</Button>
-                        <button size="large" onClick={e => handleDate(e)} value="today" className={` col-md-3 time ${date === "today" ? "selected" : ""}`}>Today // {today.toLocaleDateString('en-GB')}</button>
-                        <button size="large" onClick={e => handleDate(e)} value="tomorrow" className={` col-md-3 time ${date === "tomorrow" ? "selected" : ""}`}>Tomorrow // {tomorrow.toLocaleDateString('en-GB')}</button>
+                        <button size="large" onClick={e => handleDate(e)} value="today" className={` col-md-3 time ${date === "today" ? "selected" : ""}`}>Today - {today.toLocaleDateString('en-GB')}</button>
+                        <button size="large" onClick={e => handleDate(e)} value="tomorrow" className={` col-md-3 time ${date === "tomorrow" ? "selected" : ""}`}>Tomorrow - {tomorrow.toLocaleDateString('en-GB')}</button>
                         <button size="large" onClick={e => handleDate(e)} value="day-after" className={` col-md-3 time ${date === "day-after" ? "selected" : ""}`}>{dayAfter.toLocaleDateString('en-GB')}</button>
                     </div>
                 </div> : <></>}
@@ -57,7 +75,9 @@ const Registration = () => {
                     {arenas.map(arena => {
                         return (
                             <div key={arena.id} className="arena" onClick={e => handleClick(arena.id, (arena.name).toUpperCase())}>
-                                {(arena.name).toUpperCase()}
+                                <i className={getIcon((arena.name).toLowerCase())}></i>
+                                <h1>{(arena.name).toUpperCase()}</h1>
+                                <div className="ar-bottom"></div>
                             </div>
                         );
                     })}
