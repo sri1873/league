7e0cb2176fe9ff89b94a1c4ad52d6dd02d8d2ab1
“sri-kumar” <kssrikumar180703@gmail.com>
2023-03-12 22:19:43 +0530

    removed footer href warning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,16 @@ const Footer =()=>{
     return (
     <div className="footer-container">
         <div className="footer">
-        <div className="logo" ><img src={logo}className="logo" /></div>
+        <div className="logo" ><img alt="Woxsen Logo" src={logo}className="logo" /></div>
         <div>
         ©Copyright 2023 - Woxsen University <br/>
         <p>Designed and Developed by </p>
         <p>School of Technology</p>
-        <a target={"_blank"} href="https://www.linkedin.com/in/arunchandra-boini/">B.Arun Chandra </a>
-        <a target={"_blank"} href="https://www.linkedin.com/in/ks-srikumar/">K S Sri Kumar </a>
-        <a target={"_blank"} href="https://www.linkedin.com/in/harsh-morayya/">Harsh Morayya </a> <br/>
+        <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/arunchandra-boini/">B.Arun Chandra </a>
+        <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/ks-srikumar/">K S Sri Kumar </a>
+        <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/harsh-morayya/">Harsh Morayya </a> <br/>
         <p>Under the guidance of</p>
-        <a target={"_blank"} href="https://www.linkedin.com/in/amogh-deshmukh-35009b17/">Prof. Amogh Deshmukh</a>
+        <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/amogh-deshmukh-35009b17/">Prof. Amogh Deshmukh</a>
         </div>
         </div>
     </div>);
