a617efa700f48840e3b3de081c4c6d3f2f8288c8
“sri-kumar” <kssrikumar180703@gmail.com>
2023-03-12 22:06:36 +0530

    added footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import React from "react";
-
+import logo from '../assets/logo.png'
 const Footer =()=>{
 
     return (
     <div className="footer-container">
         <div className="footer">
-        ©Copyright - Woxsen University
-        Designed and developed by B.Arun Chandra K S Sri Kumar Harsh Morayya under the guidance of amogh sir
+        <div className="logo" ><img src={logo}className="logo" /></div>
+        <div>
+        ©Copyright 2023 - Woxsen University <br/>
+        <p>Designed and Developed by </p>
+        <p>School of Technology</p>
+        <a target={"_blank"} href="https://www.linkedin.com/in/arunchandra-boini/">B.Arun Chandra </a>
+        <a target={"_blank"} href="https://www.linkedin.com/in/ks-srikumar/">K S Sri Kumar </a>
+        <a target={"_blank"} href="https://www.linkedin.com/in/harsh-morayya/">Harsh Morayya </a> <br/>
+        <p>Under the guidance of</p>
+        <a target={"_blank"} href="https://www.linkedin.com/in/amogh-deshmukh-35009b17/">Prof. Amogh Deshmukh</a>
+        </div>
         </div>
     </div>);
 }
