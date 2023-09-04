8c364b163d2d6a217fedd84cbbc6ac74824f934d
Sri Kumar <kssrikumar180703@gmail.com>
2023-09-04 22:51:20 +0530

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,25 +11,25 @@ const Footer = () => {
                     <div class="follow">
                         {/* <h4>Explore. Discover. Connect.</h4> */}
                         <ul>
-                            <li><a href="https://www.linkedin.com/school/woxsen-university/" target="_blank">
+                            <li><a style={{textDecoration:"none"}} href="https://www.linkedin.com/school/woxsen-university/" target="_blank">
                                 <img src="https://woxsen.edu.in/assets/images/linkedin.webp" class="img-fluid" alt="icon" />
                                 <span>Linkedin</span>
                             </a>
                             </li>
-                            <li><a href="https://twitter.com/Woxsen" target="_blank">
+                            <li><a style={{textDecoration:"none"}}  href="https://twitter.com/Woxsen" target="_blank">
                                 <img src="https://woxsen.edu.in/assets/images/twitter.webp" class="img-fluid" alt="icon" />
                                 <span>Twitter</span></a>
                             </li>
-                            <li><a href="https://www.instagram.com/woxsen_university/" target="_blank">
+                            <li><a style={{textDecoration:"none"}}  href="https://www.instagram.com/woxsen_university/" target="_blank">
                                 <img src="https://woxsen.edu.in/assets/images/insta.webp" class="img-fluid" alt="icon" />
                                 <span>Instagram</span>
                             </a>
                             </li>
-                            <li><a href="https://www.youtube.com/channel/UCjh6E3poHz7IFRGpnYlQ4pQ" target="_blank">
+                            <li><a style={{textDecoration:"none"}}  href="https://www.youtube.com/channel/UCjh6E3poHz7IFRGpnYlQ4pQ" target="_blank">
                                 <img src="https://woxsen.edu.in/assets/images/youtube.webp" class="img-fluid" alt="icon" />
                                 <span>You Tube</span></a>
                             </li>
-                            <li><a href="https://woxsen.edu.in/blog/" target="_blank">
+                            <li><a style={{textDecoration:"none"}}  href="https://woxsen.edu.in/blog/" target="_blank">
                                 <img src="https://woxsen.edu.in/assets/images/blog-icon.webp" class="img-fluid" alt="icon" />
                                 <span>Blog</span></a>
                             </li>
@@ -45,11 +45,11 @@ const Footer = () => {
                     ©Copyright 2023 - Woxsen University <br />
                     <p>Designed and Developed by </p>
                     <p>School of Technology</p>
-                    <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/arunchandra-boini/">B.Arun Chandra</a> <span>| </span>
-                    <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/ks-srikumar/">K S Sri Kumar</a> <span>| </span>
-                    <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/harsh-morayya/">Harsh Morayya</a> <br />
+                    <a style={{textDecoration:"none"}}  target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/arunchandra-boini/">B.Arun Chandra</a> <span>| </span>
+                    <a style={{textDecoration:"none"}}  target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/ks-srikumar/">K S Sri Kumar</a> <span>| </span>
+                    <a style={{textDecoration:"none"}}  target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/harsh-morayya/">Harsh Morayya</a> <br />
                     <p>Under the guidance of</p>
-                    <a target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/amogh-deshmukh-35009b17/">Prof. Amogh Deshmukh</a>
+                    <a style={{textDecoration:"none"}}  target={"_blank"} rel="noopener noreferrer" href="https://www.linkedin.com/in/amogh-deshmukh-35009b17/">Prof. Amogh Deshmukh</a>
                 </div>
             </div>
 
